633eaa991bcfd807a74670ddc4633dac2ebc0c6e
Deeksha Prabhakar <deekiyengar@gmail.com>
2020-06-16 21:47:31 -0700

    chore: Updated requirements to use flask migrate to track schema changes, migrated starter code, upgraded and tested

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
 from forms import *
+from flask_migrate import Migrate
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
@@ -21,7 +22,8 @@ moment = Moment(app)
 app.config.from_object('config')
 # connect to a local postgresql database
 db = SQLAlchemy(app)
-
+# link migrate to app and the database
+migrate = Migrate(app, db)
 #----------------------------------------------------------------------------#
 # Models.
 #----------------------------------------------------------------------------#
@@ -59,7 +61,7 @@ class Artist(db.Model):
 #----------------------------------------------------------------------------#
 # Filters.
 #----------------------------------------------------------------------------#
-db.create_all()
+
 def format_datetime(value, format='medium'):
   date = dateutil.parser.parse(value)
   if format == 'full':
@@ -107,8 +109,7 @@ def venues():
       "num_upcoming_shows": 0,
     }]
   }]
-  venues=Venue.query.all()
-  return render_template('pages/venues.html', areas=venues);
+  return render_template('pages/venues.html', areas=data);
 
 @app.route('/venues/search', methods=['POST'])
 def search_venues():
