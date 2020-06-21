77b2a8b80ec553ebc07d06b49457d0e3b2d8a710
Deeksha Prabhakar <deekiyengar@gmail.com>
2020-06-21 08:33:47 -0700

    feat: sesearch artists

--- app.py
+++ app.py
@@ -315,16 +315,27 @@ def artists():
 
 @app.route('/artists/search', methods=['POST'])
 def search_artists():
-  # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-  # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
-  # search for "band" should return "The Wild Sax Band".
-  response={
-    "count": 1,
-    "data": [{
-      "id": 4,
-      "name": "Guns N Petals",
-      "num_upcoming_shows": 0,
-    }]
+  # search on artists with partial string search and is case-insensitive.
+  search = request.form['search_term']
+  data = []
+  artists = []
+  try:
+    artists = db.session.query(Artist).filter(Artist.name.ilike('%{}%'.format(search))).all()
+
+    for artist in artists:
+      data.append({
+        "id": artist.id,
+        "name": artist.name,
+        "num_upcoming_shows": len(db.session.query(Show).filter(Show.artist_id == artist.id).filter(Show.start_time > datetime.now()).all())
+      })
+  except:
+    print(sys.exc_info())
+  finally:
+    db.session.close()
+  
+  response = {
+    "count": len(artists),
+    "data": data
   }
   return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
 
